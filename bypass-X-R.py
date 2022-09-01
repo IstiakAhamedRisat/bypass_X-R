d4399293f67a0cbc27990c73f25fb184643ea3a6
IstiakRisat <105579512+IstiakAhamedRisat@users.noreply.github.com>
2022-09-01 22:02:07 +0600

    Update bypass-X-R.py

--- bypass-X-R.py
+++ bypass-X-R.py
@@ -1,19 +1,7 @@
-import os, platform
-try:os.system('git pull')
-except:pass
-try:os.mkdir('OK')
-except:pass
-try:os.mkdir('CP')
-except:pass
-try:os.system('touch .prox.txt')
-except:pass
- 
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
+
 import GREEN
 GREEN.login()
 
-elif bit == '32bit':
+
 import GREEN32
 GREEN32.login()
